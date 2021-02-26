73bb2e58ef0f0fbf1e2417157112696baf672e51
Pavel Vondruska <vondruska@cesnet.cz>
2021-02-26 14:09:44 +0100

    Added openstack_run.sh that can be used to create image within openstack

---
+++ buster/openstack_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+  
+# openstack server create --flavor standard.medium --security-group open --image debian-10-x86_64 --network auto_allocated_network --key-name dexter --config-drive true --user-data openstack_run.sh metadebian
+
+if [ ! -e /METADEBIAN ]; then
+
+set -x
+
+passwd -d root
+
+cat /home/debian/.ssh/authorized_keys > /root/.ssh/authorized_keys
+
+userdel -r -f debian
+
+apt update -qqq
+apt install -yqqq git
+
+git clone https://github.com/dexter2cz-docker/debian-workernode-docker -b devel
+cp -arv debian-workernode-docker/buster/* /tmp
+
+bash -x /tmp/create_meta_debian
+
+dpkg-reconfigure openssh-server
+
+adduser --system --shell /bin/bash --group debian
+usermod -a -G adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev debian
+
+touch /METADEBIAN
+
+cloud-init clean
+
+reboot
+fi
